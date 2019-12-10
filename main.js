0c56a1e5530332240fb740508740bcb1999b1338
Tommy Adams <thomasadams012@gmail.com>
2019-12-10 11:03:41 -0500

    objective

--- main.js
+++ main.js
@@ -36,13 +36,14 @@ const content = [`
 `,`
 <div data-aos="fade-right" data-aos-duration="700" class="col-3">
     <div class="list-group list-group-flush">
-        <a href="#_announcements" class="list-group-item list-group-item-action active">Announcements «</a>
-        <a href="#_footer" class="list-group-item list-group-item-action">Other «</a>
+        <a href="#_announcements" class="list-group-item list-group-item-action active">Why Should You Play? «</a>
+        <a href="#_footer" class="list-group-item list-group-item-action">Potential Profit «</a>
     </div>
 </div>
 <div data-aos="fade-left" data-aos-duration="700" class="col-9">
-    <h2 id="_announcements">Announcements</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean fermentum augue in nisi elementum, quis congue mauris suscipit. Ut ut dui faucibus, mollis nisi in, volutpat diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus non nisi hendrerit, molestie sem ac, dapibus nibh. Vestibulum condimentum ac justo ac vehicula. Sed rhoncus ante sit amet elit volutpat convallis. Fusce in orci a odio pellentesque faucibus ut sit amet ligula. Donec eu tellus elit. Nullam scelerisque iaculis facilisis. Suspendisse potenti. Ut vel sodales risus, varius efficitur risus.</p>
+    <h2 id="_announcements">Why Should You Play?</h2>
+    <p></br>DuoDiceM is an extremely easy game to play!</br></p>
+    <p>It is the perfect game for people looking for a relaxed (and profitable) trip to the casino!</p>
 </div>
 `,`
 <div data-aos="fade-right" data-aos-duration="700" class="col-3">
